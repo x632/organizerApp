5238c1b28704f9c78dfe27fc69b5cc18f2d21f1c
Andreas Svedstedt <andreas@poema.se>
2021-02-28 22:53:27 +0100

    All klasser gjorda med viewmodel. Login klassen inte testad ännu. Borde dock fungera.

--- app/src/main/java/com/poema/theorganizerapp/activities/Login.kt
+++ app/src/main/java/com/poema/theorganizerapp/activities/Login.kt
@@ -6,105 +6,75 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 import com.poema.theorganizerapp.R
+import com.poema.theorganizerapp.viewModels.LoginViewModel
+import com.poema.theorganizerapp.viewModels.MainActivityViewModel
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
 
-    private lateinit var db : FirebaseFirestore
-    private lateinit var auth: FirebaseAuth
+
+    private lateinit var viewModel: LoginViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        db = FirebaseFirestore.getInstance()
-        auth = FirebaseAuth.getInstance()
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val emailView = findViewById<EditText>(R.id.emailHolder)
-        val passwordView = findViewById<EditText>(R.id.passwordHolder)
-        val repeatedPasswordView = findViewById<EditText>(R.id.password2Holder)
-        val regButton = findViewById<Button>(R.id.regBtn)
-        val loginBtn = findViewById<Button>(R.id.loginBtn)
+        viewModel = ViewModelProvider(this@Login).get(LoginViewModel::class.java)
+
         //val forgotPassword = findViewById<TextView>(R.id.forgotPassword)
 
-        regButton.setOnClickListener() {
+        setIsSignedInObserver()
+        setAccountCreatedObserver()
+        setMessageObserver()
 
-            val email = emailView.text.toString()
-            val password = passwordView.text.toString()
-            val repeatedPassword = repeatedPasswordView.text.toString()
-            createAccount(email, password, repeatedPassword)
+        regBtn.setOnClickListener() {
+
+            val email = emailHolder.text.toString()
+            val password = passwordHolder.text.toString()
+            val repeatedPassword = password2Holder.text.toString()
+            viewModel.createAccount(email, password, repeatedPassword)
         }
 
         loginBtn.setOnClickListener(){
 
-            val email = emailView.text.toString()
-            val password = passwordView.text.toString()
-            login(email, password)
-        }
-        val currentUser: FirebaseUser? = auth.currentUser
-
-        if (currentUser != null) {
-            println("!!! current users uid = ${currentUser.uid}")
-            println("!!!You are signed in!")
-            toNextScreen()
-
-        } else {
-            println("!!!You are not signed in!")
+            val email = emailHolder.text.toString()
+            val password = passwordHolder.text.toString()
+            viewModel.login(email, password)
         }
 
+        val signedIn : Boolean = viewModel.checkIfSignedIn()
+        if(signedIn)toNextScreen()
     }
 
-
-    private fun login(email:String, password: String) {
-        if (email == ""  || password == ""){
-            val mess ="Authentication failed - you need to fill in both email and password"
-            showMessage(mess)
-        }
-        else {
-            auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                       toNextScreen()
-                    } else {
-                        showMessage("signInWithEmail:failure - ${task.exception}")
-                    }
-                }
-        }
+    private fun setAccountCreatedObserver() {
+        viewModel.getAccountCreated().observe(this@Login, { t ->
+            if(t){
+                toNextScreen()
+            }
+        } )
     }
+    private fun setIsSignedInObserver() {
+        viewModel.getIsSignedIn().observe(this@Login, {t->
+            if (t){
+                toNextScreen()
+            }
+        })
+    }
+    private fun setMessageObserver(){
+        viewModel.getMessage().observe(this@Login, { message ->
+            showMessage(message)
+        })
+    }
+
 
     override fun onBackPressed() {
-        Toast.makeText(this, "Cannot go back here", Toast.LENGTH_SHORT).show()
-    }
-
-
-    private fun createAccount(email:String, password:String, password2 : String) {
-        if (email == ""  || password == "" || password2 == ""){
-            val mess ="Authentication failed - none of the fields can be empty"
-            showMessage(mess)
-        }
-        else if (password != password2){
-            val mess ="Passwords do not match."
-            showMessage(mess)
-        }
-        else{
-            auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        println("createUserWithEmail:success")
-                        toNextScreen()
-
-                    } else {
-                        val eMess = "${task.exception}"
-                        val errMess = eMess.substringAfter(":")
-                        val finalMessage = "Authentication failed - $errMess"
-                        println(errMess)
-                        showMessage(finalMessage)
-                    }
-                }
-        }
+    showMessage("You can not go back here.")
     }
 
     private fun showMessage(message: String){
@@ -114,7 +84,6 @@ class Login : AppCompatActivity() {
     }
 
     private fun toNextScreen(){
-
         val intent = Intent(this, MainView::class.java)
         startActivity(intent)
     }
---
+++ app/src/main/java/com/poema/theorganizerapp/viewModels/LoginViewModel.kt
@@ -0,0 +1,77 @@
+package com.poema.theorganizerapp.viewModels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class LoginViewModel : ViewModel() {
+
+    private val auth = FirebaseAuth.getInstance()
+    private var message = MutableLiveData<String>()
+    private var isSignedIn = MutableLiveData<Boolean>()
+    private var accountCreated = MutableLiveData<Boolean>()
+
+    fun checkIfSignedIn():Boolean{
+        val currentUser: FirebaseUser? = auth.currentUser
+        return currentUser != null
+    }
+
+    fun login(email:String, password: String) {
+        if (email == ""  || password == ""){
+            message.value = "Authentication failed - you need to fill in both email and password"
+            //showMessage(mess)
+        }
+        else {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        //toNextScreen()
+                    } else {
+                        message.value = ("signInWithEmail:failure - ${task.exception}")
+                    }
+                }
+        }
+    }
+
+    fun createAccount(email:String, password:String, password2 : String) {
+        if (email == ""  || password == "" || password2 == ""){
+            message.value ="Authentication failed - none of the fields can be empty"
+            //showMessage(mess)
+        }
+        else if (password != password2){
+            message.value = "Passwords do not match."
+            //showMessage(mess)
+        }
+        else{
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        println("createUserWithEmail:success")
+                        accountCreated.value = true
+                        //toNextScreen()
+
+                    } else {
+                        val eMess = "${task.exception}"
+                        val errMess = eMess.substringAfter(":")
+                        message.value = "Authentication failed - $errMess"
+                        println(errMess)
+                        //showMessage(finalMessage)
+                    }
+                }
+        }
+    }
+
+    fun getIsSignedIn(): MutableLiveData<Boolean> {
+        return isSignedIn
+    }
+
+    fun getAccountCreated():MutableLiveData<Boolean>{
+        return accountCreated
+    }
+
+    fun getMessage():MutableLiveData<String>{
+        return message
+    }
+
+}
