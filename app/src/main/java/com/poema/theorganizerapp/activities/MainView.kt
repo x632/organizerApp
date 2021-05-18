9fc5e21bf1a4e2a5ce5d3114cc8e90a34fc6a526
Andreas Svedstedt <andreas@poema.se>
2021-05-18 16:24:34 +0200

    hamburgarmenu fixad, samt sign out, två funktioner återstår

--- app/src/main/java/com/poema/theorganizerapp/activities/MainView.kt
+++ app/src/main/java/com/poema/theorganizerapp/activities/MainView.kt
@@ -2,12 +2,17 @@ package com.poema.theorganizerapp.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.poema.theorganizerapp.R
 import com.poema.theorganizerapp.adapters.VideoAdapter
 import com.poema.theorganizerapp.utils.Utility.isInternetAvailable
@@ -21,15 +26,31 @@ import java.util.*
 
 class MainView : AppCompatActivity() {
 
+    lateinit var toggle: ActionBarDrawerToggle
     private lateinit var videoAdapter: VideoAdapter
     private lateinit var viewModel: MainViewViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_view)
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        btnDrawer.setOnClickListener{drawerLayout.openDrawer(Gravity.LEFT)}
+
 
         viewModel = ViewModelProviders.of(this, ViewModelFactory(this@MainView))
             .get(MainViewViewModel::class.java)
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.mItem1 -> { Firebase.auth.signOut()
+                    val intent = Intent(this,Login::class.java)
+                    startActivity(intent)}
+            }
+            true
+        }
         val spinner = findViewById<ProgressBar>(R.id.progressBar2)
         viewModel.getVideos()
         setGroupListObserver(spinner)
@@ -45,6 +66,10 @@ class MainView : AppCompatActivity() {
                 showToast(msg)
             }
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem):Boolean{
+        return super.onOptionsItemSelected(item)
     }
 
     private fun goToAddVideo() {
